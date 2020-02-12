50bbfb02896ef933d506938e09ee287d30b9b6af
Andrea Stacchiotti <andreastacchiotti@gmail.com>
2020-02-12 02:25:39 +0100

    Added bleed combines

---
+++ scripts/bc.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+source "$(dirname $0)/base.sh"
+
+inf=$1
+sup=$2
+table=table_bleed
+
+FOLDER=gcfw/bleed-combine
+TABLES="${table}"
+TABLE_FILES="${TABLE_DIR}/${table}"
+
+name_move() {
+	gsize=$(< tmp sed -n "${2}p" | tr -dc '0-9')
+	filename=bc$(printf "%0.7d" ${gsize})
+	echo "$filename - gemforce ${GEMFORCE_VER} - ${TABLES}" >> $1
+	mv $1 "${FOLDER}/$filename.txt"
+}
+
+for ((i=inf; i<=sup; i*=2))
+do
+	echo $i
+	
+	if [ $i -le 4096 ]
+	then
+		${GEMFORCE_DIR}/bin/bleedquery -qpe -f "${TABLE_FILES}" "$i" | tail -n +4 > tmp
+		name_move tmp 1
+		${GEMFORCE_DIR}/bin/bleedquery -qpeu -f "${TABLE_FILES}" "$i" | tail -n +9 > tmp
+		name_move tmp 3
+	else
+		${GEMFORCE_DIR}/bin/bleedquery -qe -f "${TABLE_FILES}" "$i" | tail -n +4 > tmp
+		name_move tmp 1
+		${GEMFORCE_DIR}/bin/bleedquery -qeu -f "${TABLE_FILES}" "$i" | tail -n +9 > tmp
+		name_move tmp 3
+	fi
+done
