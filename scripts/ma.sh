8dc4910e90daeaf554a8222a7cdb180097d77854
Andrea Stacchiotti <andreastacchiotti@gmail.com>
2020-02-15 01:00:47 +0100

    Updated managem amps recipes
    The optimal chain 32s is technically not reachable anymore,
    because its parent gem is pruned away, the optimal
    recipe is still displayed here.

--- scripts/ma.sh
+++ scripts/ma.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
+
+source "$(dirname $0)/base.sh"
+
 inf=$1
 sup=$2
-file=$3
+table1=$3
+table2=table_leech
+
+#QA=2
+#GA=2.5
+QA=6
+GA=1
+TC=120
+AS=60
+
+FOLDER=gccs/managem-amps
+TABLES="${table1},${table2}"
+TABLE_FILES="${TABLE_DIR}/${table1},${TABLE_DIR}/${table2}"
+
+name_move() {
+	gsize=$(< tmp sed -n '4p' | tr -dc '0-9')
+	asize=$(< tmp sed -n '11p' | tr -dc '0-9')
+	filename=ma$(printf "%0.4d" ${gsize})-${asize}
+	echo "$filename - AMPS${QA}-${GA} TC${TC} A${AS} - gemforce ${GEMFORCE_VER} - ${TABLES}" >> $1
+	mv $1 "${FOLDER}/$filename.txt"
+}
 
 for ((i=inf; i<=sup; i*=2))
 do
 	echo $i
-	filename="ma"$(printf "%0.4d" $i)
-	desc="$filename - N6 TC120 A60 - gemforce $(git describe --tags | cut -f1 -d "-") - $file"",table_leech"
-	if [ $i -le 256 ]
-	then
-		bin/mgquery-amps -rqpte -f "$file" "$i" | tail -n +26 > results/$filename".txt"
-		echo "$desc" >> results/$filename".txt"
-	else
-		bin/mgquery-amps -rqpe  -f "$file" "$i" | tail -n +26 > results/$filename".txt"
-		echo "$desc" >> results/$filename".txt"
-	fi
+	
+	${GEMFORCE_DIR}/bin/mgquery-amps -rqpe -Q$QA -G$GA -T$TC -A$AS -f "${TABLE_FILES}" "$i" | tail -n +26 > tmp
+	name_move tmp
+	${GEMFORCE_DIR}/bin/mgquery-amps -rqpeu -Q$QA -G$GA -T$TC -A$AS -f "${TABLE_FILES}" "$i" | tail -n +45 > tmp
+	name_move tmp
 done
